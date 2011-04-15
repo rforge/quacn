d37a705f5b45ae9f7dc0e65a17753b824152fe80
kuglerka <kuglerka@e88ffafe-9d54-4131-9119-39fa2b540a54>
2011-04-15 08:33:24 +0000

    QuACN 1.2.1 added getLargestSubgraph
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/quacn@23 e88ffafe-9d54-4131-9119-39fa2b540a54

---
+++ pkg/R/getSubgraphs.R
@@ -0,0 +1,12 @@
+getLargestSubgraph <- function(g){
+  if(class(g) != "graphNEL"){
+    stop("g needs to be of type graphNEL")
+  }
+  cc.g <- connectedComp(g)
+  cclens.g <- sapply(cc.g, length)
+  print("Subgraph distribution:")
+  print(table(cclens.g))
+  ord.g <- order(cclens.g, decreasing=T)
+  return(subGraph(cc.g[[ord.g[1]]], g))
+}
+
