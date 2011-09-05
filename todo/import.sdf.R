5a93433a7a475256c4842d745de63142f11769da
laurinmueller <laurinmueller@e88ffafe-9d54-4131-9119-39fa2b540a54>
2011-09-05 17:14:26 +0000

    is now working for GSK data
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/quacn@55 e88ffafe-9d54-4131-9119-39fa2b540a54

--- todo/import.sdf.R
+++ todo/import.sdf.R
@@ -8,24 +8,30 @@ importSDF <- function(filename){
   gn <- 1
   graphs <- list()
   ngs <-  length(lines[lines=="$$$$"])
-  #ngs=123
+  #ngs=23900
   while(gn<=ngs){
     if(gn%%100==0){
-      print(paste(gn,"of",ngs,sep=" "))
+      print(paste(gn,"of",ngs,"line: ",i,sep=" "))
     }
     gname <- lines[i]
-    tmp <- unlist(strsplit(lines[i+3]," "))
-    tmp <- tmp[tmp!=""]
-    nnod <- as.integer(tmp[1])
-    ned <- as.integer(tmp[2])
+#     tmp <- unlist(strsplit(lines[i+3]," "))
+#     tmp <- tmp[tmp!=""]
+#     nnod <- as.integer(tmp[1])
+#     ned <- as.integer(tmp[2])
     #create graph with nodes
-    gg = new("graphNEL", nodes=as.character(1:nnod))
+     tmp <- lines[i+3]
+     nnod <- as.integer(substr(tmp,1,3))
+     ned <- as.integer(substr(tmp,4,6))
+     gg = new("graphNEL", nodes=as.character(1:nnod))
     #create edges
     for(j in 1:ned){
-      tmp <- unlist(strsplit(lines[i+3+nnod+j]," "))
-      tmp <- tmp[tmp!=""]
-      a <- tmp[1]
-      e <- tmp[2]
+#       tmp <- unlist(strsplit(lines[i+3+nnod+j]," "))
+#       tmp <- tmp[tmp!=""]
+#       a <- tmp[1]
+#       e <- tmp[2]
+      tmp <- lines[i+3+nnod+j]
+      a <- as.character(as.integer(substr(tmp,1,3)))
+      e <- as.character(as.integer(substr(tmp,4,6)))
       gg <- addEdge(a,e,gg,1)
     }
     graphs[[paste(gname,"-GN-",gn,sep="")]] <- gg
@@ -42,9 +48,12 @@ importSDF <- function(filename){
 }
 
 file <- "/mnt/projects/DATA/MalariaCompoundsChembl/GSK/chemblntd_gsk"
-graphs <- importSDF(file)
+graphs <- importSDF(filename)
 save(graphs,file=paste(file,"RData",sep="."))
 
 file <- "/mnt/projects/DATA/MalariaCompoundsChembl/GSK/chemblntd_all"
 graphs <- importSDF(file)
 save(graphs,file=paste(file,"RData",sep="."))
+
+
+filename <- "~/Downloads/chemblntd_all"
