f3269d452e7320f0cb37001d228ba7496e04a4aa
kuglerka <kuglerka@e88ffafe-9d54-4131-9119-39fa2b540a54>
2012-09-14 10:07:39 +0000

    moved CITATION file
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/quacn@108 e88ffafe-9d54-4131-9119-39fa2b540a54

--- pkg/R/distanceMatrix.R
+++ pkg/R/distanceMatrix.R
@@ -1,4 +1,7 @@
-distanceMatrix <- function(g) {
+##g: The graphNEL object to be converted
+##keep.weights: a flag to indicate whether weights should be considered when performing the conversion (DEFAULT= FALSE)
+##currently ignored, added for later development
+distanceMatrix <- function(g, keep.weights=FALSE) {
   require("graph")
   require("RBGL")
   #print("Distance...")
@@ -6,5 +9,9 @@ distanceMatrix <- function(g) {
   if(class(g)[1]!="graphNEL"){
     stop("'g' has to be a 'graphNEL' object")
   }
+  if(any(unlist(edgeData(g, attr="weight")) >1) & keep.weights){
+    warning("When calculating distanceMatrix() the edge weight information will be ignored")
+  }
+  g <- igraph::igraph.to.graphNEL(.G2IG(g, keep.weights=FALSE))
   johnson.all.pairs.sp(g)
 }
