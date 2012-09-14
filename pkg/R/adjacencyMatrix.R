69090c44a0650f157c42b56e61b673ea6e5c3ca9
kuglerka <kuglerka@e88ffafe-9d54-4131-9119-39fa2b540a54>
2012-09-14 12:02:21 +0000

    changed adjacencyMatrix method
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/quacn@111 e88ffafe-9d54-4131-9119-39fa2b540a54

--- pkg/R/adjacencyMatrix.R
+++ pkg/R/adjacencyMatrix.R
@@ -4,5 +4,10 @@ adjacencyMatrix <- function(g){
   if(class(g)[1]!="graphNEL"){
     stop("'g' has to be a 'graphNEL' object")
   }
-  as(g, "matrix")
+  am <- as(g, "matrix")
+  if(any(am>1)){
+    warning("QuACN currently only supports unweighted graphs. The matrix will be unweighted")
+    am <- ifelse(am>1,1,am)
+  }
+  return(am)
 }
